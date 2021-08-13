f16799c541247fb081d334177d6699cb221db345
Joey Guerra <joey@joeyguerra.com>
2021-08-12 20:46:29 -0500

    trying again

--- server.mjs
+++ server.mjs
@@ -8,7 +8,7 @@ const app = express()
 express.static.mime.define({"text/javascript": ["mjs"]})
 app.use(express.static(path.join(__dirname, "./docs")))
 app.use(express.static(path.join(__dirname, "./src")))
-app.use("/lib", express.static(path.join(__dirname, "./lib")))
+app.use(express.static(path.join(__dirname, "./lib")))
 
 const listener = app.listen(process.env.PORT, () => {
   console.log(`Your app is listening on port http://localhost:${listener.address().port}`)
