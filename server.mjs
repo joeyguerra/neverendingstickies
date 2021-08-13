2f84a3689526d9ad3a78cf4b9e65923a67e24ebc
Joey Guerra <joey@joeyguerra.com>
2021-08-12 20:50:32 -0500

    fixed

--- server.mjs
+++ server.mjs
@@ -6,9 +6,9 @@ const __dirname = path.dirname(moduleURL.pathname);
 
 const app = express()
 express.static.mime.define({"text/javascript": ["mjs"]})
-app.use(express.static(path.join(__dirname, "./docs")))
-app.use(express.static(path.join(__dirname, "./src")))
-app.use(express.static(path.join(__dirname, "./lib")))
+app.use("/neverendingstickies", express.static(path.join(__dirname, "./docs")))
+app.use("/neverendingstickies/src", express.static(path.join(__dirname, "./src")))
+app.use("/neverendingstickies/lib", express.static(path.join(__dirname, "./lib")))
 
 const listener = app.listen(process.env.PORT, () => {
   console.log(`Your app is listening on port http://localhost:${listener.address().port}`)
