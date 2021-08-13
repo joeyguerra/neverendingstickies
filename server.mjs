5c4b6bab5762b37f0590b8859befdc05ec5aa71b
Joey Guerra <joey@joeyguerra.com>
2021-08-12 20:13:28 -0500

    rename public to docs

--- server.mjs
+++ server.mjs
@@ -6,7 +6,7 @@ const __dirname = path.dirname(moduleURL.pathname);
 
 const app = express()
 express.static.mime.define({"text/javascript": ["mjs"]})
-app.use(express.static(path.join(__dirname, "./public")))
+app.use(express.static(path.join(__dirname, "./docs")))
 app.use("/src", express.static(path.join(__dirname, "./src")))
 app.use("/lib", express.static(path.join(__dirname, "./lib")))
 
